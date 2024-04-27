bf9c8b8128bbde1ce582969392710d08086e748b
Mohamed15Ghaly <mohamedghaly07@gmail.com>
2024-04-27 23:30:55 +0300

    Remove unnecessary imports in prediction\_header.dart and verify.dart
    This commit removes unnecessary imports in two different files, prediction\_header.dart and verify.dart. In prediction\_header.dart, the imports

--- lib/core/components/prediction_header.dart
+++ lib/core/components/prediction_header.dart
@@ -1,7 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:team/core/utils/medi_colors.dart';
 import 'package:team/core/utils/medi_image.dart';
