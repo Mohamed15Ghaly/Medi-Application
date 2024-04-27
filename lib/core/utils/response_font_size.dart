d3eeb6cd336350691b85a222fa3e768fd081dd04
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2024-04-27 17:39:39 +0300

    modification

--- lib/core/utils/response_font_size.dart
+++ lib/core/utils/response_font_size.dart
@@ -5,19 +5,19 @@ double? getResponseFontSize(
     {required BuildContext context, required double fontSize}) {
   double? scaleFactor = getScaleFactor(context);
   double responsiveFontSize = fontSize * scaleFactor!;
-  double lowerLimit = fontSize * 1;
-  double upperLimit = fontSize * 2.75;
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
   return responsiveFontSize.clamp(lowerLimit, upperLimit);
 }
 
 double? getScaleFactor(BuildContext context) {
   double getScaleWidth = context.width;
-  if (getScaleWidth < 801) {
-    return getScaleWidth / 650;
-  } else if (getScaleWidth < 1300) {
-    return getScaleWidth / 850;
+  if (getScaleWidth < 601) {
+    return getScaleWidth / 400;
+  } else if (getScaleWidth < 900) {
+    return getScaleWidth / 700;
   } else {
-    return getScaleWidth / 1101;
+    return getScaleWidth / 1000;
   }
 }
 
